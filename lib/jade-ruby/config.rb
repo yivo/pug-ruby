729d341e4802c83aefc05ee5231be033cffa14c8
Yaroslav Konoplov <eahome00@gmail.com>
2017-08-06 22:08:17 +0300

    Cleanup configs

--- lib/jade-ruby/config.rb
+++ lib/jade-ruby/config.rb
@@ -10,36 +10,15 @@ module Jade
   # - {http://web.archive.org/web/20160618141847/http://jade-lang.com/command-line/}
   #
   class Config < JadePug::Config
-    attr_accessor :filename
-    attr_accessor :doctype
-    attr_accessor :pretty
-    attr_accessor :self
-    attr_accessor :debug
-    attr_accessor :compile_debug
-    attr_accessor :cache
-    attr_accessor :globals
-    attr_accessor :client
-    attr_accessor :name
-    attr_accessor :name_after_file
-    attr_accessor :out
-    attr_accessor :extension
-    attr_accessor :hierarchy
-
     def initialize
-      @filename        = nil
-      @doctype         = nil
-      @pretty          = false
-      @self            = false
-      @debug           = false
-      @compile_debug   = false
-      @cache           = false
-      @globals         = []
-      @client          = false
-      @name            = "template"
-      @name_after_file = nil
-      @out             = nil
-      @extension       = nil
-      @hierarchy       = false
+      super
+      @filename      = nil
+      @doctype       = nil
+      @pretty        = false
+      @self          = false
+      @compile_debug = false
+      @globals       = []
+      @name          = "template"
     end
   end
 end
--- lib/pug-ruby/config.rb
+++ lib/pug-ruby/config.rb
@@ -10,21 +10,6 @@ module Pug
   # - {https://github.com/pugjs/pug-cli}
   #
   class Config < JadePug::Config
-    attr_accessor :filename
-    attr_accessor :basedir
-    attr_accessor :doctype
-    attr_accessor :pretty
-    attr_accessor :self
-    attr_accessor :debug
-    attr_accessor :compile_debug
-    attr_accessor :globals
-    attr_accessor :inline_runtime_functions
-    attr_accessor :name
-    attr_accessor :name_after_file
-    attr_accessor :out
-    attr_accessor :extension
-    attr_accessor :silent
-
     def initialize
       super
       @filename                 = nil
@@ -32,18 +17,10 @@ module Pug
       @doctype                  = nil
       @pretty                   = false
       @self                     = false
-
-      @debug                    = false
       @compile_debug            = false
       @globals                  = []
       @inline_runtime_functions = true
-
       @name                     = "template"
-
-      @name_after_file          = nil
-      @out                      = nil
-      @extension                = nil
-      @silent                   = true
     end
   end
 end
